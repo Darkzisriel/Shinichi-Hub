546e8fd449e7bf71c45615018084967edb77d51e
DarkzDev <106386704+Darkzisriel@users.noreply.github.com>
2025-08-01 14:15:19 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,12 +5,12 @@ else
 		task.wait()
 	until game:IsLoaded()
 end
-
 local VIMVIM = game:GetService("VirtualInputManager")
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+local HttpService = game:GetService("HttpService")
 
 local DCWebhook = (getgenv and getgenv().DiscordWebhook) or false
 local GenTime = tonumber(getgenv and getgenv().GeneratorTime) or 2.5
@@ -27,19 +27,23 @@ local function CreateNotificationUI()
 	if Nnnnnnotificvationui then
 		return Nnnnnnotificvationui
 	end
+
 	Nnnnnnotificvationui = Instance.new("ScreenGui")
 	Nnnnnnotificvationui.Name = "NotificationUI"
 	Nnnnnnotificvationui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 	Nnnnnnotificvationui.Parent = game:GetService("CoreGui")
+
 	return Nnnnnnotificvationui
 end
 
 local function MakeNotif(title, message, duration, color)
 	local ui = CreateNotificationUI()
+
 	title = title or "Notification"
 	message = message or ""
 	duration = duration or 5
 	color = color or Color3.fromRGB(255, 200, 0)
+
 	local notification = Instance.new("Frame")
 	notification.Name = "Notification"
 	notification.Size = UDim2.new(0, 250, 0, 80)
@@ -47,22 +51,15 @@ local function MakeNotif(title, message, duration, color)
 	notification.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 	notification.BorderSizePixel = 0
 	notification.Parent = ui
+
 	local corner = Instance.new("UICorner")
 	corner.CornerRadius = UDim.new(0, 8)
 	corner.Parent = notification
 
-	local icon = Instance.new("ImageLabel")
-	icon.Name = "Icon"
-	icon.Size = UDim2.new(0, 24, 0, 24)
-	icon.Position = UDim2.new(0, 5, 0, 5)
-	icon.BackgroundTransparency = 1
-	icon.Image = ProfilePicture or "rbxasset://textures/ui/GuiImagePlaceholder.png"
-	icon.Parent = notification
-
 	local SIGMABERFIOENEW = Instance.new("TextLabel")
 	SIGMABERFIOENEW.Name = "Title"
-	SIGMABERFIOENEW.Size = UDim2.new(1, -35, 0, 25)
-	SIGMABERFIOENEW.Position = UDim2.new(0, 34, 0, 5)
+	SIGMABERFIOENEW.Size = UDim2.new(1, -25, 0, 25)
+	SIGMABERFIOENEW.Position = UDim2.new(0, 15, 0, 5)
 	SIGMABERFIOENEW.Font = Enum.Font.SourceSansBold
 	SIGMABERFIOENEW.Text = title
 	SIGMABERFIOENEW.TextSize = 18
@@ -91,6 +88,7 @@ local function MakeNotif(title, message, duration, color)
 	colorBar.BackgroundColor3 = color
 	colorBar.BorderSizePixel = 0
 	colorBar.Parent = notification
+
 	local barCorner = Instance.new("UICorner")
 	barCorner.CornerRadius = UDim.new(0, 8)
 	barCorner.Parent = colorBar
@@ -101,47 +99,64 @@ local function MakeNotif(title, message, duration, color)
 			offsit = offsit + notif.Instance.Size.Y.Offset + 10
 		end
 	end
+
 	local tagit = UDim2.new(1, -270, 1, -90 - offsit)
-	table.insert(AliveNotificaiotna, { Instance = notification, ExpireTime = os.time() + duration })
+
+	table.insert(AliveNotificaiotna, {
+		Instance = notification,
+		ExpireTime = os.time() + duration,
+	})
+
 	local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out)
-	game:GetService("TweenService"):Create(notification, tweenInfo, { Position = tagit }):Play()
+	local ok = game:GetService("TweenService"):Create(notification, tweenInfo, { Position = tagit })
+	ok:Play()
+
 	task.spawn(function()
 		task.wait(duration)
+
 		local tweenOut = game:GetService("TweenService"):Create(
 			notification,
 			TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.In),
 			{ Position = UDim2.new(1, 50, notification.Position.Y.Scale, notification.Position.Y.Offset) }
 		)
+
 		tweenOut:Play()
 		tweenOut.Completed:Wait()
+
 		for i, notif in pairs(AliveNotificaiotna) do
 			if notif.Instance == notification then
 				table.remove(AliveNotificaiotna, i)
 				break
 			end
 		end
+
 		notification:Destroy()
+
 		task.wait()
 		local currentOffset = 0
 		for _, notif in pairs(AliveNotificaiotna) do
 			if notif.Instance and notif.Instance.Parent then
-				game:GetService("TweenService"):Create(
-					notif.Instance,
-					TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
-					{ Position = UDim2.new(1, -270, 1, -90 - currentOffset) }
-				):Play()
+				game:GetService("TweenService")
+					:Create(
+						notif.Instance,
+						TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
+						{ Position = UDim2.new(1, -270, 1, -90 - currentOffset) }
+					)
+					:Play()
+
 				currentOffset = currentOffset + notif.Instance.Size.Y.Offset + 10
 			end
 		end
 	end)
+
 	return notification
 end
-
 
 task.spawn(function()
 	while task.wait(1) do
 		local currentTime = os.time()
 		local reposition = false
+
 		for i = #AliveNotificaiotna, 1, -1 do
 			local notif = AliveNotificaiotna[i]
 			if currentTime > notif.ExpireTime and notif.Instance and notif.Instance.Parent then
@@ -150,6 +165,7 @@ task.spawn(function()
 				reposition = true
 			end
 		end
+
 		if reposition then
 			local currentOffset = 0
 			for _, notif in pairs(AliveNotificaiotna) do
@@ -162,15 +178,19 @@ task.spawn(function()
 	end
 end)
 
-MakeNotif("Strawberry Cat Hub", "Script Loaded!", 5, Color3.fromRGB(115, 194, 89))
+MakeNotif("Gen Pathfinding Shit", "It Loaded!", 5, Color3.fromRGB(115, 194, 89))
 
 local function GetProfilePicture()
-	local PlayerID = Players.LocalPlayer.UserId
+	local PlayerID = game:GetService("Players").LocalPlayer.UserId
 	local request = request or http_request or syn.request
 	local response = request({
-		Url = "https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=" .. PlayerID .. "&size=180x180&format=png",
+		Url = "https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds="
+			.. PlayerID
+			.. "&size=180x180&format=png",
 		Method = "GET",
-		Headers = { ["User-Agent"] = "Mozilla/5.0" },
+		Headers = {
+			["User-Agent"] = "Mozilla/5.0",
+		},
 	})
 	local urlStart, urlEnd = string.find(response.Body, "https://[%w-_%.%?%.:/%+=&]+")
 	if urlStart and urlEnd then
@@ -185,55 +205,125 @@ if DCWebhook then
 end
 
 local function SendWebhook(Title, Description, Color, ProfilePicture, Footer)
-	if not DCWebhook then return end
+	if not DCWebhook then
+		return
+	end
 	local request = request or http_request or syn.request
-	pcall(function()
-		request({
+	if not request then
+		return
+	end
+
+	local success, errorMessage = pcall(function()
+		local response = request({
 			Url = DCWebhook,
 			Method = "POST",
 			Headers = { ["Content-Type"] = "application/json" },
-			Body = HttpService:JSONEncode({
-				username = Players.LocalPlayer.DisplayName,
+			Body = game:GetService("HttpService"):JSONEncode({
+				username = game:GetService("Players").LocalPlayer.DisplayName,
 				avatar_url = ProfilePicture,
-				embeds = { { title = Title, description = Description, color = Color, footer = { text = Footer } } },
+				embeds = {
+					{
+						title = Title,
+						description = Description,
+						color = Color,
+						footer = { text = Footer },
+					},
+				},
 			}),
 		})
+		if response and response.Body then
+			print(response.Body)
+		end
 	end)
-	MakeNotif("Webhook", "Sent: " .. Title .. "\n" .. Description, 5, Color3.fromRGB(115, 194, 89))
-end
 
-local function VisualizePivot(model)
-	local pivot = model:GetPivot()
-	for _, dir in ipairs({
-		{ pivot.LookVector, Color3.fromRGB(0, 255, 0) },
-		{ -pivot.LookVector, Color3.fromRGB(255, 0, 0) },
-		{ pivot.RightVector, Color3.fromRGB(255, 255, 0) },
-		{ -pivot.RightVector, Color3.fromRGB(0, 0, 255) },
-	}) do
-		local part = Instance.new("Part")
-		part.Size = Vector3.new(1, 1, 1)
-		part.Anchored = true
-		part.CanCollide = false
-		part.Color = dir[2]
-		part.Position = pivot.Position + dir[1] * 5
-		part.Parent = workspace
+	MakeNotif("PathfindGens", "Sent webhook: " .. Title .. "\n" .. Description, 5, Color3.fromRGB(115, 194, 89))
+	if not success then
+		print("Error: " .. errorMessage)
 	end
 end
 
-local function TeleportToGenerator(generator)
-	if not generator or not generator.Parent then return false end
-	if not Players.LocalPlayer.Character or not Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then return false end
-	local rootPart = Players.LocalPlayer.Character.HumanoidRootPart
-	local targetPosition = generator:GetPivot().Position + generator:GetPivot().LookVector * 3
-	VisualizePivot(generator)
-	rootPart.CFrame = CFrame.new(targetPosition)
-	task.wait(0.3)
-	if (rootPart.Position - targetPosition).Magnitude <= 5 then
-		return true
-	else
-		return false
+task.spawn(function()
+	pcall(function()
+		game:GetService("ReplicatedStorage").Modules.Network.RemoteEvent:FireServer(
+			"UpdateSettings",
+			game:GetService("Players").LocalPlayer.PlayerData.Settings.Game.MaliceDisabled,
+			true
+		)
+	end)
+end)
+
+if _G.CancelPathEvent then
+	_G.CancelPathEvent:Fire()
+end
+
+_G.CancelPathEvent = Instance.new("BindableEvent")
+
+pcall(function()
+	local Controller =
+		require(game.Players.LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("PlayerModule")):GetControls()
+	Controller:Disable()
+end)
+
+local function teleportToRandomServer()
+	local Counter = 0
+	local MaxRetry = 10
+	local RetryingDelays = 10
+
+	local Request = http_request or syn.request or request
+	if Request then
+		local url = "https://games.roblox.com/v1/games/18687417158/servers/Public?sortOrder=Asc&limit=100"
+
+		while Counter < MaxRetry do
+			local success, response = pcall(function()
+				return Request({
+					Url = url,
+					Method = "GET",
+					Headers = { ["Content-Type"] = "application/json" },
+				})
+			end)
+
+			if success and response and response.Body then
+				local data = HttpService:JSONDecode(response.Body)
+				if data and data.data and #data.data > 0 then
+					local server = data.data[math.random(1, #data.data)]
+					if server.id then
+						MakeNotif(
+							"Teleporting...",
+							"Attempting to teleport to server: " .. server.id,
+							5,
+							Color3.fromRGB(115, 194, 89)
+						)
+						task.wait(0.25)
+						TeleportService:TeleportToPlaceInstance(18687417158, server.id, Players.LocalPlayer)
+						return
+					end
+				end
+			end
+
+			Counter = Counter + 1
+			MakeNotif(
+				"PathfindGens",
+				"Retrying to get a server... Attempt " .. Counter .. "/" .. MaxRetry,
+				5,
+				Color3.fromRGB(255, 0, 0)
+			)
+			task.wait(RetryingDelays)
+		end
 	end
 end
+
+task.delay(2.5, function()
+	pcall(function()
+		local timer = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("RoundTimer").Main.Time.ContentText
+		local minutes, seconds = timer:match("(%d+):(%d+)")
+		local totalSeconds = tonumber(minutes) * 60 + tonumber(seconds)
+		print(totalSeconds .. " Left till round end.")
+		MakeNotif("PathfindGens", "Round ends in " .. totalSeconds .. " seconds.", 5, Color3.fromRGB(115, 194, 89))
+		if totalSeconds > 90 then
+			teleportToRandomServer()
+		end
+	end)
+end)
 
 local function findGenerators()
 	local folder = workspace:FindFirstChild("Map") and workspace.Map:FindFirstChild("Ingame")
@@ -242,74 +332,225 @@ local function findGenerators()
 	if map then
 		for _, g in ipairs(map:GetChildren()) do
 			if g.Name == "Generator" and g.Progress.Value < 100 then
-				table.insert(generators, g)
+				local playersNearby = false
+				for _, player in ipairs(Players:GetPlayers()) do
+					if player ~= Players.LocalPlayer and player:DistanceFromCharacter(g:GetPivot().Position) <= 25 then
+						playersNearby = true
+					end
+				end
+				if not playersNearby then
+					table.insert(generators, g)
+				end
 			end
 		end
 	end
+
 	table.sort(generators, function(a, b)
-		local p = Players.LocalPlayer
-		local c = p.Character
-		if not c or not c:FindFirstChild("HumanoidRootPart") then return false end
-		return (a:GetPivot().Position - c.HumanoidRootPart.Position).Magnitude < (b:GetPivot().Position - c.HumanoidRootPart.Position).Magnitude
+		local player = Players.LocalPlayer
+		local character = player.Character
+		if not character or not character:FindFirstChild("HumanoidRootPart") then
+			return false
+		end
+		local rootPart = character:FindFirstChild("HumanoidRootPart")
+		local aPosition = a:IsA("Model") and a:GetPivot().Position or a.Position
+		local bPosition = b:IsA("Model") and b:GetPivot().Position or b.Position
+		return (aPosition - rootPart.Position).Magnitude < (bPosition - rootPart.Position).Magnitude
 	end)
+
 	return generators
 end
 
+local function VisualizePivot(model)
+	local pivot = model:GetPivot()
+
+	for i, dir in ipairs({
+		{ pivot.LookVector, Color3.fromRGB(0, 255, 0), "Front" },
+		{ -pivot.LookVector, Color3.fromRGB(255, 0, 0), "Back" },
+		{ pivot.RightVector, Color3.fromRGB(255, 255, 0), "Right" },
+		{ -pivot.RightVector, Color3.fromRGB(0, 0, 255), "Left" },
+	}) do
+		local part = Instance.new("Part")
+		part.Size = Vector3.new(1, 1, 1)
+		part.Anchored = true
+		part.CanCollide = false
+		part.Color = dir[2]
+		part.Name = dir[3]
+		part.Position = pivot.Position + dir[1] * 5
+		part.Parent = workspace
+	end
+end
+
 local function InGenerator()
-	for _, v in ipairs(Players.LocalPlayer.PlayerGui.TemporaryUI:GetChildren()) do
-		if string.sub(v.Name,1,3)=="Gen" then
-			return false
+	for i, v in ipairs(game:GetService("Players").LocalPlayer.PlayerGui.TemporaryUI:GetChildren()) do
+		if string.sub(v.Name, 1, 3) == "Gen" then
+			return true
 		end
 	end
-	return true
+	return false
+end
+
+local function fireproximityprompt(prompt)
+	if prompt and prompt:IsA("ProximityPrompt") then
+		game:GetService("ReplicatedStorage").Modules.Network.RemoteEvent:FireServer("InputHoldBegin", prompt)
+		task.wait(0.1)
+		game:GetService("ReplicatedStorage").Modules.Network.RemoteEvent:FireServer("InputHoldEnd", prompt)
+	end
 end
 
 local function DoAllGenerators()
-	for _, g in ipairs(findGenerators()) do
-		local ok = false
-		for i=1,3 do
-			if (Players.LocalPlayer.Character:GetPivot().Position - g:GetPivot().Position).Magnitude>500 then break end
-			ok = TeleportToGenerator(g)
-			if ok then break else task.wait(1) end
-		end
-		if ok then
-			task.wait(0.5)
-			local prompt = g:FindFirstChild("Main") and g.Main:FindFirstChild("Prompt")
-			if prompt then
-				fireproximityprompt(prompt)
-				task.wait(0.5)
-				if not InGenerator() then
-					local positions={g:GetPivot().Position-g:GetPivot().RightVector*3,g:GetPivot().Position+g:GetPivot().RightVector*3}
-					for _,pos in ipairs(positions) do
-						Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(pos)
-						task.wait(0.25)
+	local currentCharacter = Players.LocalPlayer.Character
+	if not currentCharacter or not currentCharacter:FindFirstChild("HumanoidRootPart") or not currentCharacter:FindFirstChild("Humanoid") then
+		return
+	end
+	local humanoid = currentCharacter.Humanoid
+	for _, completedgen in ipairs(game.ReplicatedStorage.ObjectiveStorage:GetChildren()) do
+		local required = completedgen:GetAttribute("RequiredProgress")
+		if completedgen.Value == required then
+			SendWebhook(
+				"Generator Autofarm thing",
+				"Finished all generators, Current Balance: "
+					.. Players.LocalPlayer.PlayerData.Stats.Currency.Money.Value
+					.. "\nTime Played: "
+					.. (function()
+						local seconds = Players.LocalPlayer.PlayerData.Stats.General.TimePlayed.Value
+						local days = math.floor(seconds / (60 * 60 * 24))
+						seconds = seconds % (60 * 60 * 24)
+						local hours = math.floor(seconds / (60 * 60))
+						seconds = seconds % (60 * 60)
+						local minutes = math.floor(seconds / 60)
+						seconds = seconds % 60
+						return string.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+					end)(),
+				0x00FF00,
+				ProfilePicture,
+				".gg/fartsaken | <3"
+			)
+			task.wait(1)
+			teleportToRandomServer()
+			return
+		else
+			for _, gen in ipairs(workspace.Map.Ingame:WaitForChild("Map"):GetChildren()) do
+				if gen.Name == "Generator" and gen.Progress.Value < 100 then
+					local goalPos = gen:WaitForChild("Positions").Right.Position
+					humanoid:MoveTo(goalPos)
+					humanoid.MoveToFinished:Wait()
+					local prompt = gen.Main:FindFirstChild("Prompt")
+					if prompt then
+						prompt.HoldDuration = 0
+						prompt.RequiresLineOfSight = false
+						prompt.MaxActivationDistance = 99999
+						task.wait(0.1)
 						fireproximityprompt(prompt)
-						if InGenerator() then break end
+						task.wait(0.1)
+						while gen.Progress.Value < 100 do
+							fireproximityprompt(prompt)
+							gen.Remotes.RE:FireServer()
+							task.wait(GenTime)
+						end
 					end
 				end
 			end
-			for i=1,6 do
-				if g.Progress.Value<100 and g:FindFirstChild("Remotes") and g.Remotes:FindFirstChild("RE") then
-					g.Remotes.RE:FireServer()
-				end
-				if i<6 and g.Progress.Value<100 then task.wait(GenTime) end
-			end
-		else
-			return
 		end
 	end
-	SendWebhook("Generator Autofarm","Finished all generators.",0x00FF00,ProfilePicture,".gg/CEyArUEnNW | By Darkz X Shinichi")
+	SendWebhook(
+		"Generator Autofarm thing",
+		"Finished all generators, Current Balance: "
+			.. Players.LocalPlayer.PlayerData.Stats.Currency.Money.Value
+			.. "\nTime Played: "
+			.. (function()
+				local seconds = Players.LocalPlayer.PlayerData.Stats.General.TimePlayed.Value
+				local days = math.floor(seconds / (60 * 60 * 24))
+				seconds = seconds % (60 * 60 * 24)
+				local hours = math.floor(seconds / (60 * 60))
+				seconds = seconds % (60 * 60)
+				local minutes = math.floor(seconds / 60)
+				seconds = seconds % 60
+				return string.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds)
+			end)(),
+		0x00FF00,
+		ProfilePicture,
+		".gg/fartsaken | <3"
+	)
 	task.wait(1)
+	teleportToRandomServer()
 end
 
-local function AmIInGameYet()
-	workspace.Players.Survivors.ChildAdded:Connect(function(child)
-		task.wait(1)
-		if child==Players.LocalPlayer.Character then
-			task.wait(4)
-			DoAllGenerators()
+local isInGame = false
+
+task.spawn(function()
+	while true do
+		local Spectators = {}
+		for i, child in ipairs(workspace.Players.Spectating:GetChildren()) do
+			table.insert(Spectators, child.Name)
 		end
-	end)
-end
+		if table.find(Spectators, Players.LocalPlayer.Name) then
+			isInGame = false
+		else
+			isInGame = true
+		end
+		task.wait(1)
+	end
+end)
 
-AmIInGameYet()
+local busy = false
+local isSprinting = false
+local stamina = 0
+
+task.spawn(function()
+	while true do
+		if isInGame then
+			local success, err = pcall(function()
+				local currentCharacter = Players.LocalPlayer.Character
+				if currentCharacter and currentCharacter:FindFirstChild("Humanoid") then
+					currentCharacter.Humanoid:SetAttribute("BaseSpeed", 14)
+					local barText = Players.LocalPlayer.PlayerGui.TemporaryUI.PlayerInfo.Bars.Stamina.Amount.Text
+					stamina = tonumber(string.split(barText, "/")[1])
+					local isSprintingFOV = currentCharacter.FOVMultipliers.Sprinting.Value == 1.125
+					if not isSprintingFOV then
+						if stamina >= 70 then
+							if busy then
+								return
+							end
+							VIMVIM:SendKeyEvent(true, Enum.KeyCode.LeftShift, false, game)
+						end
+					end
+				end
+			end)
+			if not success then
+				warn("Error during sprint check:", err)
+			end
+		end
+		task.wait(1)
+	end
+end)
+
+task.spawn(function()
+	task.wait(20 * 60)
+	TeleportService:Teleport(game.PlaceId)
+end)
+
+while true do
+	if isInGame then
+		local currentCharacter
+		for _, surv in ipairs(workspace.Players.Survivors:GetChildren()) do
+			if surv:GetAttribute("Username") == Players.LocalPlayer.Name then
+				currentCharacter = surv
+			end
+		end
+		if currentCharacter and currentCharacter:FindFirstChild("Humanoid") then
+			task.spawn(function()
+				while true do
+					if currentCharacter.Humanoid.Health <= 0 then
+						isInGame = false
+						isSprinting = false
+						busy = false
+						break
+					end
+					task.wait(0.5)
+				end
+			end)
+		end
+		DoAllGenerators()
+	end
+	task.wait(0.1)
+end
